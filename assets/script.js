625a9e46a7c529a79ede6f58953765687aee5fe4
Sczygelski <avamariescz@gmail.com>
2022-06-26 17:17:32 -0500

    added date

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+//$("#currentDay")
+    //let now = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+let dt = document.getElementById("currentDay");
+dt.innerHTML = moment().format('YYYY-MM-DD h:mm:ss');
