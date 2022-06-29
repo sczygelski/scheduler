200d6f35707e96c03c2985ca84a2a647400ca0d2
Sczygelski <avamariescz@gmail.com>
2022-06-28 20:41:41 -0500

    saving where Im at

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,23 @@
-//$("#currentDay")
-    //let now = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("09").val(09)
 
 let dt = document.getElementById("currentDay");
 dt.innerHTML = moment().format('YYYY-MM-DD h:mm:ss');
+
+ 
+//coloring
+
+    var hr = moment().hour();
+        if(hr>9) {
+            console.log("works");
+            //$('#timecomp').addClass("future");
+        }
+
+
+//saving
+$("#saveBtn").on("click",function(e) {
+    e.preventDefault();
+    var input = $("#savBtn").val();
+    console.log(input);
+
+})
+
