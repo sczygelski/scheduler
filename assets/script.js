f2a845f703b61a1fd3487f6b1f3e368596d092a7
Sczygelski <avamariescz@gmail.com>
2022-06-29 19:04:40 -0500

    cleaning up and updating data-hour

--- assets/script.js
+++ assets/script.js
@@ -9,14 +9,14 @@ dt.innerHTML = moment().format('YYYY-MM-DD h:mm:ss');
  
 //coloring
 const time0 = $('#time0');
-const timeten = $('#timeten');
-const timeeleven = $('#timeeleven');
-const timetwelve = $('#timetwelve');
-const timeone = $('#timeone');
-const timetwo = $('#timetwo');
-const timethree = $('#timethree');
-const timefour = $('#timefour');
-const timefive = $('#timefive');
+const time1 = $('#time1');
+const time2 = $('#time2');
+const time3 = $('#time3');
+const time4 = $('#time4');
+const time5 = $('#time5');
+const time6 = $('#time6');
+const time7 = $('#time7');
+const time8 = $('#time8');
 
 if(currenthour > 9) {
     time0.addClass("past");
@@ -29,83 +29,83 @@ else if(currenthour < 9) {
 }
 
 if(currenthour > 10) {
-    timeten.addClass("past");
+    time1.addClass("past");
 }
 else if(currenthour = 10) {
-    timeten.addClass("present");
+    time1.addClass("present");
 }
 else if(currenthour < 10) {
-    timeten.addClass("future");
+    time1.addClass("future");
 }
 
 if(currenthour > 11) {
-    timeeleven.addClass("past");
+    time2.addClass("past");
 }
 else if(currenthour = 11) {
-    timeeleven.addClass("present");
+    time2.addClass("present");
 }
 else if(currenthour < 11) {
-    timeeleven.addClass("future");
+    time2.addClass("future");
 }
 
 if(currenthour > 12) {
-    timetwelve.addClass("past");
+    time3.addClass("past");
 }
 else if(currenthour = 12) {
-    timetwelve.addClass("present");
+    time3.addClass("present");
 }
 else if(currenthour < 12) {
-    timetwelve.addClass("future");
+    time3.addClass("future");
 }
 
 if(currenthour > 13) {
-    timeone.addClass("past");
+    time4.addClass("past");
 }
 else if(currenthour = 13) {
-    timeone.addClass("present");
+    time4.addClass("present");
 }
 else if(currenthour < 13) {
-    timeone.addClass("future");
+    time4.addClass("future");
 }
 
 if(currenthour > 14) {
-    timetwo.addClass("past");
+    time5.addClass("past");
 }
 else if(currenthour = 14) {
-    timetwo.addClass("present");
+    time5.addClass("present");
 }
 else if(currenthour < 14) {
-    timetwo.addClass("future");
+    time5.addClass("future");
 }
 
 if(currenthour > 15) {
-    timethree.addClass("past");
+    time6.addClass("past");
 }
 else if(currenthour = 15) {
-    timethree.addClass("present");
+    time6.addClass("present");
 }
 else if(currenthour < 15) {
-    timethree.addClass("future");
+    time6.addClass("future");
 }
 
 if(currenthour > 16) {
-    timefour.addClass("past");
+    time7.addClass("past");
 }
 else if(currenthour = 16) {
-    timefour.addClass("present");
+    time7.addClass("present");
 }
 else if(currenthour < 16) {
-    timefour.addClass("future");
+    time7.addClass("future");
 }
 
 if(currenthour > 17) {
-    timefive.addClass("past");
+    time8.addClass("past");
 }
 else if(currenthour = 17) {
-    timefive.addClass("present");
+    time8.addClass("present");
 }
 else if(currenthour < 17) {
-    timefive.addClass("future");
+    time8.addClass("future");
 }
 
 //saving
@@ -125,17 +125,8 @@ const btn = document.querySelectorAll('.saveBtn');
 const note = document.getElementById('note');
 console.log(btn)
 for (let i = 0; i < btn.length; i++) {
-    console.log(btn[i])    
-
     document.getElementById(`time${i}`).value = localStorage.getItem(`time${i}`)
-
-
     btn[i].addEventListener('click', function(event) {
-        // const text = 'Appointment has been saved!';
-        // note.innerHTML=text;
-        console.log('farm')
-        console.log(document.getElementById(event.target.getAttribute("data-hour")).value)
-        console.log(event.target.getAttribute("data-hour"))
         localStorage.setItem(event.target.getAttribute("data-hour"), document.getElementById(event.target.getAttribute("data-hour")).value)
     })
 }
